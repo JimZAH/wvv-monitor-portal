7fc0a59ea5b322a94a83c6d4c4b38fcc6be571ab
JimZAH <james@colderwood.com>
2022-04-04 19:11:12 +0100

    Test html template

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -16,6 +17,12 @@ var rd = redis.NewClient(&redis.Options{
 	Password: "",
 	DB:       0,
 })
+
+func testpl(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	t, _ := template.ParseFiles("template/testpl.html")
+	t.Execute(w, nil)
+}
 
 func xlx(w http.ResponseWriter, r *http.Request) {
 
@@ -56,6 +63,7 @@ func xlx(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/xlx", xlx)
+	http.HandleFunc("/test", testpl)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
