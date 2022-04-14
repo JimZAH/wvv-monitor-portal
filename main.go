daacaf136e022eb5eef5321bb66ba0d6f2f50513
JimZAH <james@colderwood.com>
2022-04-14 21:36:19 +0100

    Sort nodes by connection time

--- main.go
+++ main.go
@@ -203,6 +203,8 @@ func xlxNodesJson(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal([]byte(nodes), &node)
 
+	sort.Slice(node, func(i, j int) bool { return node[i].ConnectTime < node[j].ConnectTime })
+
 	json.NewEncoder(w).Encode(node)
 
 }
